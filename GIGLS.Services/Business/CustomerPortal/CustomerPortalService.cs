c25bfa6e767073eec7b3a454bb9c3203e22aee54
ayeke.ezekiel <ayeke.ezekiel@thegiggroupng.com>
2018-05-13 13:45:28 +0100

    CustomerPortal additions

--- GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
+++ GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
@@ -6,24 +6,68 @@ using GIGLS.Core.DTO.Account;
 using GIGLS.Core.DTO.Shipments;
 using GIGLS.Core.DTO.Wallet;
 using GIGLS.CORE.DTO.Report;
+using GIGLS.Core.IServices.Shipments;
+using GIGLS.Core;
+using System.Linq;
+using AutoMapper;
+using GIGLS.Core.IServices.Account;
+using GIGLS.Core.IServices.Business;
 
 namespace GIGLS.Services.Business.CustomerPortal
 {
     public class CustomerPortalService : ICustomerPortalService
     {
-        public Task<List<ShipmentDTO>> GetShipmentTransactions(ShipmentFilterCriteria f_Criteria)
+        private readonly IUnitOfWork _uow;
+        private readonly IShipmentService _shipmentService;
+        private readonly IInvoiceService _invoiceService;
+        private readonly IShipmentTrackService _iShipmentTrackService;
+
+        public CustomerPortalService(IUnitOfWork uow, IShipmentService shipmentService, IInvoiceService invoiceService,
+            IShipmentTrackService iShipmentTrackService)
         {
-            throw new NotImplementedException();
+            _shipmentService = shipmentService;
+            _invoiceService = invoiceService;
+            _iShipmentTrackService = iShipmentTrackService;
+            _uow = uow;
+            MapperConfig.Initialize();
         }
-        
+
+
+        public async Task<List<ShipmentDTO>> GetShipmentTransactions(ShipmentFilterCriteria f_Criteria)
+        {
+            var shipments = _uow.Shipment.GetAll();
+
+            //filter by CustomerId
+            if (f_Criteria.CustomerId > 0)
+            {
+                shipments = shipments.Where(s => s.CustomerId == f_Criteria.CustomerId);
+            }
+
+            //filter by CustomerType
+            if (f_Criteria.FilterCustomerType != null)
+            {
+                shipments = shipments.Where(s => s.CustomerType == f_Criteria.FilterCustomerType.ToString());
+            }
+
+            //filter by UserId
+            if (f_Criteria.UserId != null)
+            {
+                shipments = shipments.Where(s => s.UserId == f_Criteria.UserId);
+            }
+
+            var shipmentsDto = Mapper.Map<List<ShipmentDTO>>(shipments.ToList());
+            return await Task.FromResult(shipmentsDto);
+        }
+
         public Task<WalletTransactionSummaryDTO> GetWalletTransactions()
         {
             throw new NotImplementedException();
         }
 
-        public Task<InvoiceDTO> GetInvoiceByWaybill(string waybill)
+        public async Task<InvoiceDTO> GetInvoiceByWaybill(string waybill)
         {
-            throw new NotImplementedException();
+            var invoice = await _invoiceService.GetInvoiceByWaybill(waybill);
+            return invoice;
         }
 
         public Task<IEnumerable<InvoiceDTO>> GetInvoices()
@@ -31,9 +75,10 @@ namespace GIGLS.Services.Business.CustomerPortal
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ShipmentTrackingDTO>> TrackShipment(string waybillNumber)
+        public async Task<IEnumerable<ShipmentTrackingDTO>> TrackShipment(string waybillNumber)
         {
-            throw new NotImplementedException();
+            var result = await _iShipmentTrackService.TrackShipment(waybillNumber);
+            return result;
         }
 
         public Task<CashOnDeliveryAccountSummaryDTO> GetCashOnDeliveryAccount()
