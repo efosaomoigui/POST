d3f81e793089341ca6225ce57400869ee58b5d8b
Akpohre <egborge.akporhe@thegiggroupng.com>
2019-03-07 16:46:55 +0100

    Changes to BankShipment

--- GIGLS.Services/Implementation/BankSettlement/BankShipmentSettlementService.cs
+++ GIGLS.Services/Implementation/BankSettlement/BankShipmentSettlementService.cs
@@ -455,7 +455,7 @@ namespace GIGLS.Services.Implementation.Wallet
                     var serviceCenters = _userService.GetPriviledgeServiceCenters().Result;
                     var allCODs = _uow.CashOnDeliveryRegisterAccount.GetCODAsQueryable();
                     //allCODs = allCODs.Where(s => s.DateCreated >= startdate && s.DateCreated <= enddate);
-                    allCODs = allCODs.Where(s => s.DepositStatus == DepositStatus.Unprocessed);
+                    allCODs = allCODs.Where(s => s.DepositStatus == DepositStatus.Unprocessed && s.PaymentType == PaymentType.Cash);
                     var codsforservicecenter = allCODs.Where(s => serviceCenters.Contains(s.ServiceCenterId)).ToList();
 
                     var result1 = codsforservicecenter.Select(s => new CashOnDeliveryRegisterAccountDTO()
