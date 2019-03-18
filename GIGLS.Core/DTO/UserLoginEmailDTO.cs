620fd256f2b87e657b5b45822612c29604e4634f
Ayeke Ezekiel <ayeke.ezekiel@thegiggroupng.com>
2019-03-18 13:14:58 +0100

    User Login Email Interval Fix

---
+++ GIGLS.Core/DTO/UserLoginEmailDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GIGLS.Core.DTO
+{
+    public class UserLoginEmailDTO
+    {
+        public string Email { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastSent { get; set; }
+        public int NumberOfEmailsSent { get; set; }
+    }
+}
