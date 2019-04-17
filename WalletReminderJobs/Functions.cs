8b687f06f11fcd1a5df4a886700b434b34116cb0
Efe20x <omoigui.efosa@thegiggroupng.com>
2019-04-17 15:46:47 +0100

    new on jobs

--- WalletReminderJobs/Functions.cs
+++ WalletReminderJobs/Functions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,11 +9,14 @@ namespace WalletReminderJobs
 {
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        // This function will be triggered based on the schedule you have set for this WebJob
+        // This function will enqueue a message on an Azure Queue called queue
+        [NoAutomaticTrigger]
+        public static void ManualTrigger(TextWriter log, int value, [Queue("queue")] out string message)
         {
-            log.WriteLine(message);
+            log.WriteLine("Function is invoked with value={0}", value);
+            message = value.ToString();
+            log.WriteLine("Following message will be written on the Queue={0}", message);
         }
     }
 }
