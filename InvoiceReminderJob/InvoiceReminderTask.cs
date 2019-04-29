4cc442e350a469255761e9d1430953585dd64a6c
Efosa <omoigui.efosa@thegiggroupng.com>
2019-04-29 17:39:08 +0100

    commitJobs

---
+++ InvoiceReminderJob/InvoiceReminderTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiProcessor;
+using Microsoft.Azure.WebJobs;
+
+namespace InvoiceReminderJob
+{
+    class InvoiceReminderTask 
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            try
+            {
+                ApiCaller.callAPI("GET", "invoice/sendemailfordueinvoices?datetoduedate=14  ", Console.Out).Wait();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error calling your.0 api: " + ex.ToString());
+            }
+        }
+    }
+}
---
+++ ReprintReminder/ReprintReminderTask.cs
@@ -0,0 +1,23 @@
+﻿using ApiProcessor;
+using System;
+using System.Configuration;
+
+namespace ReprintReminderJob
+{
+    class ReprintReminderTask 
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            try
+            {
+                ApiCaller.callAPI("GET", "api/webjobs/runreprintexpirycounter", Console.Out).Wait();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error calling your.0 api: " + ex.ToString());
+            }
+        }
+    }
+}
