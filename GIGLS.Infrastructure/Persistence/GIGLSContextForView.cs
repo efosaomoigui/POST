c34cea6946f20ac5183781522922abc0b7586c5a
Efe20x <omoigui.efosa@thegiggroupng.com>
2019-05-23 17:08:15 +0100

    commmit_timeout issues

--- GIGLS.Infrastructure/Persistence/GIGLSContextForView.cs
+++ GIGLS.Infrastructure/Persistence/GIGLSContextForView.cs
@@ -3,6 +3,8 @@ using GIGLS.INFRASTRUCTURE.SoftDeleteHandler;
 using GIGLS.Core.View;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using GIGLS.Core.Domain.Wallet;
+using System.Configuration;
+using System;
 
 namespace GIGLS.Infrastructure.Persistence
 {
@@ -14,6 +16,20 @@ namespace GIGLS.Infrastructure.Persistence
         {
             Database.SetInitializer<GIGLSContextForView>(null);
             Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
+            //Get Bank Deposit Module StartDate
+            var isTimeOutCheck = ConfigurationManager.AppSettings["isTimeOut"];
+            var timeForTimeOut = ConfigurationManager.AppSettings["AgilityTimeOutVal"];
+
+            if (isTimeOutCheck != null && isTimeOutCheck == "true")
+            {
+                if (timeForTimeOut != null)
+                {
+                    var outVal = Convert.ToInt32(timeForTimeOut);
+                    Database.CommandTimeout = outVal;
+                }
+            }
+
         }
 
         public DbSet<InvoiceView> InvoiceView { get; set; }
