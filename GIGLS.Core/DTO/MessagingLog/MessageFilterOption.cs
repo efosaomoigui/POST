aa1e7fafa81ae60ce431826a6435acb640e26b0b
Oladejo <bab.oladejo@student.oauife.edu.ng>
2019-07-15 17:55:35 +0100

    filter sms and email log

--- GIGLS.Core/DTO/MessagingLog/MessageFilterOption.cs
+++ GIGLS.Core/DTO/MessagingLog/MessageFilterOption.cs
@@ -8,5 +8,56 @@ namespace GIGLS.Core.DTO.MessagingLog
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public MessagingLogStatus? Status { get; set; }
+
+
+        /// <summary>
+        /// Get the Start Date and End Date for query to the database
+        /// </summary>
+        /// <param name="filterCriteria"></param>
+        /// <returns></returns>
+        public Tuple<DateTime, DateTime> getStartDateAndEndDate()
+        {
+            MessageFilterOption filterCriteria = this;
+
+            var startDate = DateTime.Now;
+            var endDate = DateTime.Now;
+
+            //If No Date Supplied
+            if (!filterCriteria.StartDate.HasValue && !filterCriteria.EndDate.HasValue)
+            {
+                startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+                endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            }
+
+            //StartDate has value and EndDate has Value 
+            if (filterCriteria.StartDate.HasValue && filterCriteria.EndDate.HasValue)
+            {
+                var tempStartDate = ((DateTime)filterCriteria.StartDate);
+                startDate = new DateTime(tempStartDate.Year, tempStartDate.Month, tempStartDate.Day);
+
+                var tempEndDate = ((DateTime)filterCriteria.EndDate);
+                endDate = new DateTime(tempEndDate.Year, tempEndDate.Month, tempEndDate.Day);
+            }
+
+            //StartDate has value and EndDate has no Value
+            if (filterCriteria.StartDate.HasValue && !filterCriteria.EndDate.HasValue)
+            {
+                var tempStartDate = ((DateTime)filterCriteria.StartDate);
+                startDate = new DateTime(tempStartDate.Year, tempStartDate.Month, tempStartDate.Day);
+
+                endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            }
+
+            //StartDate has no value and EndDate has Value
+            if (filterCriteria.EndDate.HasValue && !filterCriteria.StartDate.HasValue)
+            {
+                startDate = new DateTime(2000, DateTime.Now.Month, DateTime.Now.Day);
+
+                var tempEndDate = ((DateTime)filterCriteria.EndDate);
+                endDate = new DateTime(tempEndDate.Year, tempEndDate.Month, tempEndDate.Day);
+            }
+
+            return new Tuple<DateTime, DateTime>(startDate, endDate.AddDays(1));
+        }
     }
 }
