39707b4a2647777e58e415ec98c661b4c8c55ccb
GIG LOGISTICS <it@giglogistics.com>
2019-07-22 16:42:52 +0100

    increase and decrease percentage

--- GIGLS.Core/DTO/Zone/WeightPercentDTO.cs
+++ GIGLS.Core/DTO/Zone/WeightPercentDTO.cs
@@ -7,6 +7,7 @@ namespace GIGLS.Core.DTO.Zone
         public string Category { get; set; }
         public string PriceType { get; set; }
         public string CustomerType { get; set; }
+        public string ModificationType { get; set; }
         public decimal WeightOne { get; set; }
         public decimal WeightTwo { get; set; }
         public decimal WeightThree { get; set; }
