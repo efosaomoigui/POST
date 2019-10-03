7af592046c5f2ae811dbeee59c2c6613d29d3069
Omolola Omolambe <omolola_intern@thegiggroupng.com>
2019-10-03 11:45:09 +0100

    Delivery Location Price APIs

--- GIGLS.Core/IServices/IDeliveryLocationService.cs
+++ GIGLS.Core/IServices/IDeliveryLocationService.cs
@@ -7,5 +7,10 @@ namespace GIGLS.Core.IServices
     public interface IDeliveryLocationService : IServiceDependencyMarker
     {
         Task<IEnumerable<DeliveryLocationDTO>> GetDeliveryLocations();
+        Task<object> AddDeliveryLocationPrice(DeliveryLocationDTO deliveryLocationDTO);
+        Task UpdateDeliveryLocationPrice(int deliveryLocationId, DeliveryLocationDTO deliveryLocationDTO);
+        Task RemoveDeliveryLocationPrice(int deliveryLocationId);
+        Task<DeliveryLocationDTO> GetDeliveryLocationById(int deliveryLocationId);
+
     }
 }
