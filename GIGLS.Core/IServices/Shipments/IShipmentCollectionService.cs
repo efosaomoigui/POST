05d6c05464f64a10d2cc42aaf15e2c9ce5842440
Oladejo <bab.oladejo@student.oauife.edu.ng>
2019-10-15 16:37:50 +0100

    fix overdue shipments

--- GIGLS.Core/IServices/Shipments/IShipmentCollectionService.cs
+++ GIGLS.Core/IServices/Shipments/IShipmentCollectionService.cs
@@ -1,6 +1,7 @@
 ﻿using GIGLS.Core.DTO.Report;
 using GIGLS.Core.IServices;
 using GIGLS.CORE.DTO.Shipments;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -19,16 +20,16 @@ namespace GIGLS.CORE.IServices.Shipments
         Task ReleaseShipmentForCollection(ShipmentCollectionDTO shipmentCollection);        
         Task RemoveShipmentCollection(string waybill);
         Task CheckShipmentCollection(string waybill);
-        System.Tuple<Task<List<ShipmentCollectionDTO>>, int> GetOverDueShipments(FilterOptionsDto filterOptionsDto);
-        System.Tuple<Task<List<ShipmentCollectionDTO>>, int> GetEcommerceOverDueShipments(FilterOptionsDto filterOptionsDto);
+        Task<Tuple<List<ShipmentCollectionDTO>, int>> GetOverDueShipments(FilterOptionsDto filterOptionsDto);
+        Task<Tuple<List<ShipmentCollectionDTO>, int>> GetEcommerceOverDueShipments(FilterOptionsDto filterOptionsDto);
 
-        System.Tuple<Task<List<ShipmentCollectionDTO>>, int> GetOverDueShipmentsGLOBAL(FilterOptionsDto filterOptionsDto);
-        System.Tuple<Task<List<ShipmentCollectionDTO>>, int> GetEcommerceOverDueShipmentsGLOBAL(FilterOptionsDto filterOptionsDto);
+        Task<Tuple<List<ShipmentCollectionDTO>, int>> GetOverDueShipmentsGLOBAL(FilterOptionsDto filterOptionsDto);
+        Task<Tuple<List<ShipmentCollectionDTO>, int>> GetEcommerceOverDueShipmentsGLOBAL(FilterOptionsDto filterOptionsDto);
 
         Task<IEnumerable<ShipmentCollectionDTO>> GetEcommerceOverDueShipmentsGLOBAL();
         Task<IEnumerable<ShipmentCollectionDTO>> GetEcommerceOverDueShipments();
 
-        System.Tuple<Task<List<ShipmentCollectionDTO>>, int> GetShipmentWaitingForCollectionForHub(FilterOptionsDto filterOptionsDto);
+        Task<Tuple<List<ShipmentCollectionDTO>, int>> GetShipmentWaitingForCollectionForHub(FilterOptionsDto filterOptionsDto);
 
         Task AddRiderToDeliveryTable(ShipmentCollectionDTO shipmentCollection);
 
