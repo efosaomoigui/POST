61f55c50c3331c3c6dc850618e255aee76a103d4
Oladejo <bab.oladejo@student.oauife.edu.ng>
2019-11-16 15:28:58 +0100

    reverse otp sms to twilio

--- GIGLS.Services/Implementation/OTPService.cs
+++ GIGLS.Services/Implementation/OTPService.cs
@@ -94,7 +94,7 @@ namespace GIGLS.Services.Implementation
                 SenderEmail = user.EmailAddress,
                 SenderPhoneNumber = user.PhoneNumber,
                 OTP = user.Otp,
-                SMSSenderPlatform = SMSSenderPlatform.OGOSMS
+                SMSSenderPlatform = SMSSenderPlatform.TWILIO
             };
 
             var response = await _messageSenderService.SendMessage(MessageType.OTP, EmailSmsType.All, message);
