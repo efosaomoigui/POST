53b773042f33fa8ec6159a080b2c306608bbc996
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-21 11:47:09 +0100

    optimise and filter service centre to exclude hub for some service centre

--- GIGLS.WebApi/Controllers/ServiceCentres/ServiceCentreController.cs
+++ GIGLS.WebApi/Controllers/ServiceCentres/ServiceCentreController.cs
@@ -257,5 +257,23 @@ namespace GIGLS.WebApi.Controllers.ServiceCentres
             });
         }
 
+
+        [GIGLSActivityAuthorize(Activity = "View")]
+        [HttpGet]
+        [Route("servicecentrewithouthubfornonlagosstation/{countryId:int}")]
+        public async Task<IServiceResponse<IEnumerable<ServiceCentreDTO>>> GetServiceCentresWithoutHUBForNonLagosStation(int countryId)
+        {
+            return await HandleApiOperationAsync(async () =>
+            {
+                //2. priviledged users service centres
+                var usersServiceCentresId = await _userService.GetPriviledgeServiceCenters();
+
+                var centres = await _service.GetServiceCentresWithoutHUBForNonLagosStation(usersServiceCentresId[0], countryId);
+                return new ServiceResponse<IEnumerable<ServiceCentreDTO>>
+                {
+                    Object = centres
+                };
+            });
+        }
     }
 }
