5d9388a6826fd872f7e53f922934dcc543e44884
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-05-22 11:33:40 +0100

    add status code to shipment creation

--- GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
+++ GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
@@ -125,7 +125,7 @@ namespace GIGLS.Services.Implementation.Shipments
                     message = $"You are not yet eligible to create shipment on the Platform. You need a minimum balance of {preShipment.CurrencySymbol}{carPickUprice.Value}, please fund your wallet with additional {preShipment.CurrencySymbol}{remainingamount} to complete the process. Thank you";
                     newPreShipment.Waybill = "";
 
-                    throw new GenericException(message);
+                    throw new GenericException(message, $"{(int)HttpStatusCode.Forbidden}");
                 }
                 return new { waybill = newPreShipment.Waybill, message = message, IsBalanceSufficient, Zone = zoneid.ZoneId };
             }
