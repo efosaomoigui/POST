86cce4baf5ec1322d521fcb8943db0b30802b584
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-07-02 12:45:02 +0100

    fix pickup time issue

--- GIGLS.Core/DTO/Shipments/PreShipmentMobileDTO.cs
+++ GIGLS.Core/DTO/Shipments/PreShipmentMobileDTO.cs
@@ -150,8 +150,8 @@ namespace GIGLS.Core.DTO.Shipments
         public decimal ShipmentPickupPrice { get; set; }
         public PartnerDTO partnerDTO { get; set; }
 
-        public DateTime TimePickedUp { get; set; }
-        public DateTime TimeDelivered { get; set; }
+        public DateTime? TimePickedUp { get; set; }
+        public DateTime? TimeDelivered { get; set; }
 
     }
     public class NewPreShipmentMobileDTO : BaseDomainDTO
