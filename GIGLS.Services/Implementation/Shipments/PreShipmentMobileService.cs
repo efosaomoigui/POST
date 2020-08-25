8f0b2868c27a2364b2ded1c6414e43222adb0883
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-08-25 13:14:56 +0100

    fix the pricing issue

--- GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
+++ GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
@@ -1301,7 +1301,7 @@ namespace GIGLS.Services.Implementation.Shipments
                 var IsWithinProcessingTime = await WithinProcessingTime(preShipment.CountryId);
                 var returnprice = new MobilePriceDTO()
                 {
-                    MainCharge = (decimal)calculatedTotal,
+                    MainCharge = mainCharge,
                     DeliveryPrice = preShipment.DeliveryPrice,
                     Vat = 0.0M,
                     PickUpCharge = pickuprice,
