2c31396c6716162e04fefd7c4a8fe404c5abb480
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-11-25 18:02:09 +0100

    fix unknown error for node api call

--- GIGLS.Services/Business/Node/NodeService.cs
+++ GIGLS.Services/Business/Node/NodeService.cs
@@ -1,8 +1,11 @@
 ﻿using GIGLS.Core.DTO.Node;
 using GIGLS.Core.IServices.Node;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http;
+using System.Text;
 
 namespace GIGLS.Services.Business.Node
 {
@@ -12,12 +15,21 @@ namespace GIGLS.Services.Business.Node
         {
             try
             {
+                var nodeURL = ConfigurationManager.AppSettings["NodeMerchartBaseUrl"];
+                var nodePostShipment = ConfigurationManager.AppSettings["NodeWalletLoaded"];
+                nodeURL = nodeURL + nodePostShipment;
+
+                var dic = new Dictionary<string, string>
+                {
+                    { "email",  user.Email},
+                    { "userId", user.UserId }
+                };
+
                 using (var client = new HttpClient())
                 {
-                    var nodeURL = ConfigurationManager.AppSettings["NodeMerchartBaseUrl"];
-                    var nodePostShipment = ConfigurationManager.AppSettings["NodeWalletLoaded"];
-                    nodeURL = nodeURL + nodePostShipment;
-                    client.PostAsJsonAsync(nodeURL, user);
+                    var json = JsonConvert.SerializeObject(dic);
+                    var data = new StringContent(json, Encoding.UTF8, "application/json");
+                    var response = client.PostAsync(nodeURL, data);
                 }
             }
             catch (Exception)
