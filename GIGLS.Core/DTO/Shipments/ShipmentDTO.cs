dd65b2e90c89056c71cb6cde34562d3a608d7c19
Collette Chizaram <obiakor.chizaram@giglogistics.ng>
2020-12-07 09:37:50 +0100

    modification added business name

--- GIGLS.Core/DTO/Shipments/ShipmentDTO.cs
+++ GIGLS.Core/DTO/Shipments/ShipmentDTO.cs
@@ -324,5 +324,6 @@ namespace GIGLS.Core.DTO.Shipments
         public bool IsCOD { get; set; }
         public string ShipmentStatus { get; set; }
         public string ShipmentScanStatus { get; set; }
+        public string CompanyName { get; set; }
     }
 }
