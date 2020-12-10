15469250caa348e32c344a6d76f9b99364d8ef29
Collette Chizaram <obiakor.chizaram@giglogistics.ng>
2020-12-10 10:27:48 +0100

    added try catch

--- GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
+++ GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
@@ -433,30 +433,35 @@ namespace GIGLS.Services.Implementation.Wallet
             var url = "https://api.paystack.co/bank/resolve_bvn/";
             url = $"{url}{bvnNo}";
             var liveSecretKey = "sk_live_7b183fa191d0fddf1d0682346a5ceeeed66a52e9";
-            await Task.Run(async () =>
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            try
             {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri(url);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Add("Authorization", $"bearer {liveSecretKey}");
-                response = await client.GetAsync(url);
-            });
+                await Task.Run(async () =>
+                   {
+                       HttpClient client = new HttpClient();
+                       client.BaseAddress = new Uri(url);
+                       client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                       client.DefaultRequestHeaders.Add("Authorization", $"Bearer {liveSecretKey}");
+                       response = await client.GetAsync(url);
+                   });
 
-            if (response.IsSuccessStatusCode)
-            {
                 string jObject = await response.Content.ReadAsStringAsync();
                 var content = JsonConvert.DeserializeObject<BVNVerificationDTO>(jObject);
+               
                 result.Succeeded = content.Status;
+                result.Exist = content.Status;
                 result.Message = content.Message;
                 result.Entity = content.Data;
+             
             }
-            else
+            catch (Exception ex)
             {
-                result.Succeeded = false;
-                result.Message = "unable to connect to server";
+
+                throw;
             }
             return result;
         }
+
 
         //Ghana Wallet Payment
         private async Task<bool> ProcessPaymentForWallet(PaystackWebhookDTO webhook)
