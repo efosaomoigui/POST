af6c5211921436c2dc7410f91499c05f8f383823
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-01-27 09:22:19 +0100

    change visa bonus message

--- GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
+++ GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
@@ -930,7 +930,7 @@ namespace GIGLS.Services.Implementation.Wallet
 
             var message = new MessageDTO()
             {
-                Subject = "Visa Business Card Bonus",
+                Subject = "Visa Commercial Card Bonus",
                 CustomerCode = walletDto.CustomerEmail,  
                 CustomerName = walletDto.CustomerName,
                 Body = body                
@@ -968,7 +968,7 @@ namespace GIGLS.Services.Implementation.Wallet
                     if (isPresent)
                     {
                         result.Amount = await CalculateCardBonus(result.Amount, countryId);
-                        result.Description = $"{result.Description}. Bonus Added for Using {verifyResult.data.Authorization.CardType}";
+                        result.Description = $"{result.Description}. Bonus Added for using Visa Commercial Card";
                         result.BonusAdded = true;
                     }
                 }
