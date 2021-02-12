7f1218f7f1eac1b6d83923f3c28b67f810029169
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-02-12 17:24:19 +0100

    add archive entities

---
+++ GIGLS.Core/Domain/Archived/BaseDomain_Archive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GIGLS.Core.Domain.Archived
+{
+    public class BaseDomain_Archive
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [MaxLength(20)]
+        public string RowVersion { get; set; }
+    }
+}
---
+++ GIGLS.Core/Domain/Archived/ShipmentItem_Archive.cs
@@ -0,0 +1,25 @@
+﻿using GIGLS.Core.Enums;
+
+namespace GIGLS.Core.Domain.Archived
+{
+    public class ShipmentItem_Archive : BaseDomain_Archive
+    {
+        public int ShipmentItemId { get; set; }
+        public string Description { get; set; }
+        public string Description_s { get; set; }
+        public ShipmentType ShipmentType { get; set; }
+        public double Weight { get; set; }
+        public string Nature { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int SerialNumber { get; set; }
+        public int ShipmentPackagePriceId { get; set; }
+        public int PackageQuantity { get; set; }
+        public bool IsVolumetric { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public int ShipmentId { get; set; }
+        public virtual Shipment_Archive Shipment { get; set; }
+    }
+}
