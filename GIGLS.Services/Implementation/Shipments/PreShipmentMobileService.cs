4a991628bcc46eff1b3db09a3b29348e9034e42a
Collette Chizaram <obiakor.chizaram@giglogistics.ng>
2021-02-17 13:15:58 +0100

    progress report completed

--- GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
+++ GIGLS.Services/Implementation/Shipments/PreShipmentMobileService.cs
@@ -6321,9 +6321,7 @@ namespace GIGLS.Services.Implementation.Shipments
             {
                 var dateFor3Hours = DateTime.Now.AddHours(-3);
                 var preShipmentAssigned = _uow.PreShipmentMobile.GetAllAsQueryable().Where(x => x.shipmentstatus == "Assigned for Pickup" && x.DateModified < dateFor3Hours).ToList();
-                var dtos = Mapper.Map<List<PreShipmentMobileDTO>>(preShipmentAssigned);
-                dtos = await AssignPartnerInfo(dtos);
-                return dtos;
+                return Mapper.Map<List<PreShipmentMobileDTO>>(preShipmentAssigned);
             }
             catch (Exception ex)
             {
@@ -6336,9 +6334,7 @@ namespace GIGLS.Services.Implementation.Shipments
             {
                 var dateFor3Hours = DateTime.Now.AddHours(-3);
                 var preShipmentPicked = _uow.PreShipmentMobile.GetAllAsQueryable().Where(x => x.shipmentstatus == "PickedUp" && x.DateModified < dateFor3Hours).ToList();
-                var dtos = Mapper.Map<List<PreShipmentMobileDTO>>(preShipmentPicked);
-                dtos = await AssignPartnerInfo(dtos);
-                return dtos;
+                return Mapper.Map<List<PreShipmentMobileDTO>>(preShipmentPicked);
             }
             catch (Exception ex)
             {
@@ -6346,50 +6342,5 @@ namespace GIGLS.Services.Implementation.Shipments
             }
         }
 
-
-        public async Task<List<PreShipmentMobileDTO>> AssignPartnerInfo(List<PreShipmentMobileDTO> preShipmentMobiles)
-        {
-            try
-            {
-                if (preShipmentMobiles == null)
-                {
-                    throw new GenericException("Invalid payload");
-                }
-                //GET ASSIGNED PARTNER
-                HttpClient client = new HttpClient();
-                foreach (var item in preShipmentMobiles)
-                {
-                    var nodeURL = ConfigurationManager.AppSettings["NodeBaseUrl"];
-                    var url = ConfigurationManager.AppSettings["NodePostShipment"];
-                    nodeURL = $"{nodeURL}/api/{url}/waybillNumber={item.Waybill}";
-                    //   nodeURL = $"http://nodeapi.gigl-go.com/api/shipment/1349034538";
-                    HttpResponseMessage response = await client.GetAsync(nodeURL);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string jObject = await response.Content.ReadAsStringAsync();
-                        var result = JsonConvert.DeserializeObject<DataInfo>(jObject);
-                        if (result.PartnerInfo.assignedPartner != null)
-                        {
-                            var partnerInfo = await _uow.Partner.GetPartnerByUserId(result.PartnerInfo.assignedPartner);
-                            if (partnerInfo != null)
-                            {
-                                item.PartnerFirstName = partnerInfo.PartnerName;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        continue;
-                    }
-
-                }
-                return preShipmentMobiles;
-
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
     }
 };
