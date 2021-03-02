85ce5b8e5c7ca95964351256e67f78b5b0d8473b
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-03-02 16:17:41 +0100

    optimise the database entity

--- GIGLS.Core/Domain/ReferrerCode.cs
+++ GIGLS.Core/Domain/ReferrerCode.cs
@@ -1,13 +1,18 @@
-﻿namespace GIGLS.Core.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GIGLS.Core.Domain
 {
     public class ReferrerCode : BaseDomain, IAuditable
     {
         public int ReferrerCodeId { get; set; }
 
+        [MaxLength(50)]
         public string Referrercode { get; set; }
 
+        [MaxLength(128)]
         public string UserId { get; set; }
 
+        [MaxLength(100)]
         public string UserCode { get; set; }
     }
 }
