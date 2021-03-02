e00818fe7e03942b7731b72cfb72d17ed1b7e94f
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-03-02 14:41:42 +0100

    update domain

--- GIGLS.Core/Domain/Workshop.cs
+++ GIGLS.Core/Domain/Workshop.cs
@@ -1,15 +1,23 @@
 ﻿using GIGLS.Core;
 using GIGLS.Core.Domain;
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GIGL.GIGLS.Core.Domain
 {
     public class Workshop : BaseDomain, IAuditable
     {
         public int WorkshopId { get; set; }
+
+        [MaxLength(100)]
         public string WorkshopName { get; set; }
+
+        [MaxLength(500)]
         public string Address { get; set; }
+
+        [MaxLength(100)]
         public string City { get; set; }
+
+        [MaxLength(100)]
         public string State { get; set; }
         public virtual User WorkshopSupervisor { get; set; }
     }
