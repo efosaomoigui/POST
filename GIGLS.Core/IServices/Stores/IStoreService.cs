ee311cb6cbc4d551b152d7cdc266ddea1c12e66f
Collette Chizaram <obiakor.chizaram@giglogistics.ng>
2021-03-08 16:53:26 +0100

    store imp ongoing

--- GIGLS.Core/IServices/Stores/IStoreService.cs
+++ GIGLS.Core/IServices/Stores/IStoreService.cs
@@ -1,11 +1,12 @@
 ﻿using GIGLS.Core.DTO.Stores;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GIGLS.Core.IServices.Stores
 {
     public interface IStoreService : IServiceDependencyMarker
     {
-        Task<StoreDTO> GetStores();
+        Task<IEnumerable<StoreDTO>> GetStores();
         Task<StoreDTO> GetStoreById(int storeId);
         Task<object> AddStore(StoreDTO store);
         Task UpdateStore(int storeId, StoreDTO store);
