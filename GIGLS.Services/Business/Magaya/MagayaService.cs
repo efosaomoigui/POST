73e042a0b44294ea3b9577ca6ef5a136bea223ab
Collette Chizaram <obiakor.chizaram@giglogistics.ng>
2021-03-11 16:23:02 +0100

    vat  fix

--- GIGLS.Services/Business/Magaya/MagayaService.cs
+++ GIGLS.Services/Business/Magaya/MagayaService.cs
@@ -1020,12 +1020,12 @@ namespace GIGLS.Services.Business.Magaya.Shipments
                 var RequestNumber = await _numberGeneratorMonitorService.GenerateNextNumber(NumberGeneratorType.RequestNumber, destinationServiceCenter.Code);
                 shipmentDTO.RequestNumber = RequestNumber;
 
+                if (shipmentDTO.RequestProcessingCountryId == 0)
+                {
+                    shipmentDTO.RequestProcessingCountryId = 207;
+                }
                 if (shipmentDTO.Consolidated)
                 {
-                    if (shipmentDTO.RequestProcessingCountryId == 0)
-                    {
-                        shipmentDTO.RequestProcessingCountryId = 207;
-                    }
                     //get count of all unprocessed consolited item
                     var consolidated = _uow.IntlShipmentRequest.GetAll().Where(x => x.UserId == user.Id && x.Consolidated == true && x.IsProcessed == false && x.RequestProcessingCountryId == shipmentDTO.RequestProcessingCountryId).OrderBy(x => x.DateCreated).ToList();
                     count = consolidated.Count;
@@ -1050,14 +1050,6 @@ namespace GIGLS.Services.Business.Magaya.Shipments
                 newShipment.DestinationCountryId = Convert.ToInt32(station.Country);
                 newShipment.ReceiverCountry = shipmentDTO.ReceiverCountry;
                
-                if (shipmentDTO.RequestProcessingCountryId <= 0)
-                {
-                    var country = await _uow.Country.GetAsync(x => x.CountryId == user.UserActiveCountryId);
-                    if (country != null)
-                    {
-                        newShipment.RequestProcessingCountryId = country.CountryId;
-                    }
-                }
                 string itemName = "";
 
                 var serialNumber = 1;
