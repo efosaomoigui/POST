9b6146fec1672d4caf0a4f4b755c779112bf1875
Efosa Omoigui <omoigui.efosa@thegiggroupng.com>
2021-04-08 11:17:43 +0100

    movement manifest corrections

--- GIGLS.Services/Implementation/Shipments/ShipmentService.cs
+++ GIGLS.Services/Implementation/Shipments/ShipmentService.cs
@@ -2324,7 +2324,7 @@ namespace GIGLS.Services.Implementation.Shipments
                 var movementManifest = await _uow.MovementManifestNumber.FindAsync(x => x.MovementManifestCode == movementManifestCode);
                 var ManifestNumber = movementManifest.FirstOrDefault();
 
-                if (ManifestNumber.IsDriverValid == true && ManifestNumber.MovementStatus != MovementStatus.EnRoute)
+                if (ManifestNumber.IsDriverValid == false && ManifestNumber.MovementStatus != MovementStatus.EnRoute)
                 {
                     return true;
                 }
@@ -2352,7 +2352,7 @@ namespace GIGLS.Services.Implementation.Shipments
                     if (ManifestNumber.DriverCode == valMovementManifest.code)
                     {
                         ManifestNumber.IsDriverValid = true;
-                        ManifestNumber.MovementStatus = MovementStatus.InProgress;
+                        ManifestNumber.MovementStatus = MovementStatus.EnRoute;
                         await _uow.CompleteAsync();
                         retVal = true;
                     }
