72a836c938893edffb2a40eaadb011f5bca9ead4
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-04-08 13:58:23 +0100

    fix the payment conflict issue

--- GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
+++ GIGLS.Services/Implementation/Wallet/PaystackPaymentSErvice.cs
@@ -1267,10 +1267,14 @@ namespace GIGLS.Services.Implementation.Wallet
         private bool ValidatePaymentValue(decimal shipmentAmount, decimal paystackAmount)
         {
             var factor = Convert.ToDecimal(Math.Pow(10, 0));
-            paystackAmount = Math.Round(paystackAmount * factor) / factor;
-            shipmentAmount = Math.Round(shipmentAmount * factor) / factor;
+            decimal roundUpPaystackAmount = Math.Round(paystackAmount * factor) / factor;
 
-            if (shipmentAmount == paystackAmount)
+            decimal increaseShipmentPrice = shipmentAmount + 1;
+            decimal decreaseShipmentPrice = shipmentAmount - 1;
+
+            if (shipmentAmount == paystackAmount
+                || increaseShipmentPrice == roundUpPaystackAmount
+                || decreaseShipmentPrice == roundUpPaystackAmount)
             {
                 return true;
             }
