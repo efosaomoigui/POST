9f3a9686231c44d5db56566bd2c174244d3ff348
Oladejo <bab.oladejo@student.oauife.edu.ng>
2021-04-12 11:56:07 +0100

    bind the response from ups

--- GIGLS.Core/DTO/UPS/UPSShipmentResponsePayload.cs
+++ GIGLS.Core/DTO/UPS/UPSShipmentResponsePayload.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GIGLS.Core.DTO.UPS
 {
+    public class UPSShipmentResponseFinalPayload
+    {
+        public UPSShipmentResponsePayload ShipmentResponse { get; set; }
+        public FaultPayload Fault { get; set; }
+    }
+
+    //success response
     public class UPSShipmentResponsePayload
     {
         public UPSShipmentResponsePayload()
         {
-            ShipmentResponse = new UPSShipmentResponse();
+            Response = new UPSShipmentResponse();
             ShipmentResults = new UPSShipmentResults();
         }
-        public UPSShipmentResponse ShipmentResponse { get; set; }
+        public UPSShipmentResponse Response { get; set; }
         public UPSShipmentResults ShipmentResults { get; set; }
     }
 
@@ -49,6 +57,8 @@ namespace GIGLS.Core.DTO.UPS
         public UPSShipmentCharges ShipmentCharges { get; set; }
         public UPSPackaging BillingWeight { get; set; }
         public string ShipmentIdentificationNumber { get; set; }
+
+        //the result can be an object and array too
         public List<UPSPackageResults> PackageResults { get; set; }
     }
 
@@ -91,6 +101,66 @@ namespace GIGLS.Core.DTO.UPS
         }
         public LabelImageFormat ImageFormat { get; set; }
         public string GraphicImage { get; set; }
-        public string HTMLImage { get; set; }        
+        public string HTMLImage { get; set; }
     }
+
+    //failed Response for UPS
+
+    public class FaultPayload
+    {
+        public FaultPayload()
+        {
+            Detail = new FaultDetail();
+        }
+
+        [JsonProperty("faultcode")]
+        public string FaultCode { get; set; }
+
+        [JsonProperty("faultstring")]
+        public string FaultString { get; set; }
+
+        public FaultDetail Detail { get; set; }
+    }
+
+    public class FaultDetail
+    {
+        public FaultDetail()
+        {
+            Errors = new UPSErrors();
+        }
+
+        public UPSErrors Errors { get; set; }
+    }
+
+    public class UPSErrors
+    {
+        public UPSErrors()
+        {
+            ErrorDetail = new DetailErrors();
+        }
+        public DetailErrors ErrorDetail { get; set; }
+    }
+
+    public class DetailErrors
+    {
+        public DetailErrors()
+        {
+            PrimaryErrorCode = new UPSPrimaryErrorCode();
+        }
+
+        [JsonProperty("Severity")]
+        public string Severity { get; set; }
+
+        public UPSPrimaryErrorCode PrimaryErrorCode { get; set; }
+    }
+
+    public class UPSPrimaryErrorCode
+    {
+        [JsonProperty("Code")]
+        public string Code { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+    }
+
 }
--- GIGLS.Services/Business/UPS/UPSService.cs
+++ GIGLS.Services/Business/UPS/UPSService.cs
@@ -2,10 +2,12 @@
 using GIGLS.Core.DTO.Shipments;
 using GIGLS.Core.DTO.UPS;
 using GIGLS.Core.IServices.UPS;
+using GIGLS.Infrastructure;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -22,21 +24,34 @@ namespace GIGLS.Services.Business.UPS
             return result;
         }
 
-        //yet to be implemented
-        private InternationalShipmentWaybillDTO FormatShipmentCreationReponse(UPSShipmentResponsePayload upsResponse)
+        private InternationalShipmentWaybillDTO FormatShipmentCreationReponse(UPSShipmentResponseFinalPayload upsResponse)
         {
             InternationalShipmentWaybillDTO output = new InternationalShipmentWaybillDTO();
 
-           // if(upsResponse.ShipmentResponse.re)
+            if(upsResponse != null)
+            {
+                throw new GenericException($"There was an issue processing your request: " +
+                    $"{upsResponse.Fault.Detail.Errors.ErrorDetail.PrimaryErrorCode.Description}");
+            }
+            else
+            {
+                output.ShipmentIdentificationNumber = upsResponse.ShipmentResponse.ShipmentResults.ShipmentIdentificationNumber;
+
+                if (upsResponse.ShipmentResponse.ShipmentResults.PackageResults.Any())
+                {
+                    string[] itemIds = upsResponse.ShipmentResponse.ShipmentResults.PackageResults.Select(x => x.TrackingNumber).ToArray();
+                    output.PackageResult = string.Join(",", itemIds);
+                }
+            }
 
             return output;
         }
 
-        public async Task<UPSShipmentResponsePayload> CreateUPSShipment(InternationalShipmentDTO shipmentDto)
+        public async Task<UPSShipmentResponseFinalPayload> CreateUPSShipment(InternationalShipmentDTO shipmentDto)
         {
             try
             {
-                var result = new UPSShipmentResponsePayload();
+                var result = new UPSShipmentResponseFinalPayload();
 
                 //Get Price from DHL
                 var request = GetShipmentRequestPayload(shipmentDto);
@@ -54,7 +69,7 @@ namespace GIGLS.Services.Business.UPS
                     StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
                     HttpResponseMessage response = await client.PostAsync(url, data);
                     string responseResult = await response.Content.ReadAsStringAsync();
-                    result = JsonConvert.DeserializeObject<UPSShipmentResponsePayload>(responseResult);
+                    result = JsonConvert.DeserializeObject<UPSShipmentResponseFinalPayload>(responseResult);
                 }
 
                 return result;
