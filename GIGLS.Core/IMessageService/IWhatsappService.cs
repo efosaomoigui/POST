ae41017f4e658e978ad8188ed16dde262c2620da
Olanrewaju-Samuel <olanrewaju.samuel@giglogistics.ng>
2021-06-17 11:49:22 +0100

    Whatsapp services class added

---
+++ GIGLS.Core/IMessageService/IWhatsappService.cs
@@ -0,0 +1,13 @@
+﻿using GIGLS.Core.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIGLS.Core.IMessageService
+{
+    public interface IWhatsappService : IServiceDependencyMarker
+    {
+    }
+}
---
+++ GIGLS.Messaging/MessageService/WhatsappService.cs
@@ -0,0 +1,14 @@
+﻿using GIGLS.Core.IMessageService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIGLS.Messaging.MessageService
+{
+    public class WhatsappService : IWhatsappService
+    {
+
+    }
+}
