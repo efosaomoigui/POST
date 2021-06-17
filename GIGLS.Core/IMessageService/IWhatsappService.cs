851160f0ca8132ce5798ab087716464c9282a2d2
Olanrewaju-Samuel <olanrewaju.samuel@giglogistics.ng>
2021-06-17 12:03:15 +0100

    method definitions added

--- GIGLS.Core/IMessageService/IWhatsappService.cs
+++ GIGLS.Core/IMessageService/IWhatsappService.cs
@@ -1,4 +1,5 @@
-﻿using GIGLS.Core.IServices;
+﻿using GIGLS.Core.DTO;
+using GIGLS.Core.IServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,8 @@ namespace GIGLS.Core.IMessageService
 {
     public interface IWhatsappService : IServiceDependencyMarker
     {
+        Task<string> SendWhatsappMessageAsync(WhatsAppMessageDTO message);
+        Task<string> GetConsentDetailsAsync(WhatsappNumberDTO number);
+        Task<string> ManageOptInOutAsync(ManageWhatsappConsentDTO consent);
     }
 }
