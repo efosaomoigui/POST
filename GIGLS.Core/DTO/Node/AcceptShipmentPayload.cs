5eb961d2629c7300b0e2a9d26a3d9a412d39ef8f
Olanrewaju-Samuel <olanrewaju.samuel@giglogistics.ng>
2021-07-08 13:58:41 +0100

    AcceptShipmentPayload

---
+++ GIGLS.Core/DTO/Node/AcceptShipmentPayload.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIGLS.Core.DTO.Node
+{
+    public class AcceptShipmentPayload
+    {
+        [JsonProperty("waybillNumber")]
+        public string WaybillNumber { get; set; }
+
+        [JsonProperty("partnerId")]
+        public string PartnerId { get; set; }
+
+        [JsonProperty("partnerInfo")]
+        public PartnerPayload PartnerInfo { get; set; }
+    }
+}
