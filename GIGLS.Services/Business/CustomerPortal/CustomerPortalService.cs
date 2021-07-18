873df8d062867e6947d3ed14e460c5bc9ecc9b6e
Samuel Olanrewaju <olanrewaju.samuel@giglogistics.ng>
2021-07-18 14:45:36 +0100

    remove + if phone number contains +

--- GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
+++ GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
@@ -3545,6 +3545,12 @@ namespace GIGLS.Services.Business.CustomerPortal
                 throw new GenericException("Phone number is required");
             }
 
+            whatsappNumber.PhoneNumber = whatsappNumber.PhoneNumber.Trim();
+
+            if (whatsappNumber.PhoneNumber.Contains("+"))
+            {
+                whatsappNumber.PhoneNumber = whatsappNumber.PhoneNumber.Replace("+", string.Empty);
+            }
             await _messageSenderService.ManageOptInOutForWhatsappNumber(whatsappNumber);
 
             return true;
