e4cbafcf84e996b6fcf29055d17ef3dc0db71c75
Collette-Chizaram <obiakor.chizaram@giglogistics.ng>
2021-09-19 10:07:08 +0100

    added manifest to querry param

--- GIGLS.Services/Implementation/Partnership/PartnerService.cs
+++ GIGLS.Services/Implementation/Partnership/PartnerService.cs
@@ -602,7 +602,7 @@ namespace GIGLS.Services.Implementation.Partnership
                         pagination.EndDate = pagination.EndDate.Value.AddHours(23).AddMinutes(59);
                     }
                     var captainIds = captains.Select(x => x.Id).ToList();
-                    var walletTransactions = _uow.PartnerTransactions.GetAllAsQueryable().Where(x => captainIds.Contains(x.UserId) && x.DateCreated >= pagination.StartDate && x.DateCreated <= pagination.EndDate).GroupBy(x => x.UserId).ToList();
+                    var walletTransactions = _uow.PartnerTransactions.GetAllAsQueryable().Where(x => captainIds.Contains(x.UserId) && x.DateCreated >= pagination.StartDate && x.DateCreated <= pagination.EndDate && !String.IsNullOrEmpty(x.Manifest)).GroupBy(x => x.UserId).ToList();
                     if (walletTransactions.Any())
                     {
                         transactions = (from r in walletTransactions
