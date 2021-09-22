c2552314e1e2d2e8f54b79b78c9533bea4cf8484
Emmatex <emmatex4yall@hotmail.com>
2021-09-22 23:07:08 +0100

    update

--- GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
+++ GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
@@ -1944,7 +1944,7 @@ namespace GIGLS.Services.Business.CustomerPortal
         {
             // check for special characters
             //Regex rgx  new Regex(@"^[a-zA-Z0-9]\d{2}[a-zA-Z0-9](-\d{3}){2}[A-Za-z0-9]$");
-            var valid =await CheckSpecialCharacters(waybillNumber);
+            var valid = await CheckSpecialCharacters(waybillNumber);
             if (valid)
             {
                 throw new GenericException($"Invalid waybill number, special characters not allowed", $"{(int)HttpStatusCode.Forbidden}");
@@ -2947,7 +2947,7 @@ namespace GIGLS.Services.Business.CustomerPortal
                 }
                 else
                 {
-                    throw new GenericException("Waybill  Not Found", $"{(int)HttpStatusCode.NotFound}"); 
+                    throw new GenericException("Waybill  Not Found", $"{(int)HttpStatusCode.NotFound}");
                 }
             }
             if (invoice.PaymentStatus == PaymentStatus.Paid)
@@ -3393,7 +3393,7 @@ namespace GIGLS.Services.Business.CustomerPortal
                     {
                         throw new GenericException($"No price definition for this category", $"{(int)HttpStatusCode.BadRequest}");
                     }
-                   
+
                     if (itemCategory.SubminimumWeight > 0 && quickQuotePriceDTO.Weight <= itemCategory.SubminimumWeight)
                     {
                         for (int i = 1; i <= quickQuotePriceDTO.Quantity; i++)
@@ -3849,7 +3849,7 @@ namespace GIGLS.Services.Business.CustomerPortal
 
 
         public async Task<IEnumerable<PriceCategoryDTO>> GetPriceCategoriesBothCountries(int destcountryId, int deptcountryId)
-        {         
+        {
             var categories = await _uow.PriceCategory.GetPriceCategoriesByCountryId(destcountryId, deptcountryId);
             return Mapper.Map<List<PriceCategoryDTO>>(categories);
 
@@ -3885,7 +3885,7 @@ namespace GIGLS.Services.Business.CustomerPortal
                 decimal discount = 0.0M;
                 var amount = await _preShipmentMobileService.CalculateBikePriceBasedonLocation(preShipment);
 
-                decimal pickuprice = 0.0M;  
+                decimal pickuprice = 0.0M;
                 decimal pickupValue = 0.0M;
                 decimal mainCharge = basePriceBikeValue + amount;
                 decimal percentage = 0.0M;
@@ -3961,21 +3961,26 @@ namespace GIGLS.Services.Business.CustomerPortal
         {
             try
             {
+                if (couponDto.CouponCodeValue < 1)
+                {
+                    throw new GenericException($"Coupon value must be greater than one", $"{(int)HttpStatusCode.BadRequest}");
+                }
                 var couponList = new List<CouponCodeManagement>();
                 var couponCodes = await GenerateCouponCode(couponDto.Number);
                 foreach (var code in couponCodes)
                 {
                     var coupon = JObject.FromObject(couponDto).ToObject<CouponCodeManagement>();
                     coupon.CouponCode = code;
+                    couponDto.IsCouponCodeUsed = false;
                     couponList.Add(coupon);
                 }
-                 _uow.CouponManagement.AddRange(couponList);
+                _uow.CouponManagement.AddRange(couponList);
                 _uow.Complete();
                 return couponCodes;
             }
             catch (Exception ex)
             {
-                throw; 
+                throw;
             }
         }
 
