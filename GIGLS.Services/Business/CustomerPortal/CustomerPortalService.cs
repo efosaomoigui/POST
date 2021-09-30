522875851967a8cd693ea287f13a96f7124fa4ff
Collette-Chizaram <obiakor.chizaram@giglogistics.ng>
2021-09-30 17:05:57 +0100

    add changes

--- GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
+++ GIGLS.Services/Business/CustomerPortal/CustomerPortalService.cs
@@ -4117,6 +4117,10 @@ namespace GIGLS.Services.Business.CustomerPortal
 
                 }
             }
+            var desc = (serviceSMS.BillType == BillType.TVSUB) ? "Payment for TV subcription" : (serviceSMS.BillType == BillType.ELECTRICITY) ? "Payment for electricity unity Top up"
+                    : (serviceSMS.BillType == BillType.AIRTIME) ? "Payment for airtime Top up" : (serviceSMS.BillType == BillType.DATASUB) ? "Payment for data subcription" : "Customer subscription";
+					
+					
             //send message for service rendered
             var messageDTO = new MessageDTO
             {
