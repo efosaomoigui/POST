02d25c3d8299bd42902e2799edc1f569cfa4ab7b
Collette-Chizaram <obiakor.chizaram@giglogistics.ng>
2022-02-15 12:24:57 +0100

    add

--- GIGLS.Services/Business/Magaya/MagayaService.cs
+++ GIGLS.Services/Business/Magaya/MagayaService.cs
@@ -1002,7 +1002,7 @@ namespace GIGLS.Services.Business.Magaya.Shipments
                 var customer = await _customerService.GetCustomer(user.UserChannelCode, user.UserChannelType);
                 int count = 0;
 
-                if (shipmentDTO.DeliveryType == DeliveryType.GOSTANDARDED)
+                if (shipmentDTO.DeliveryType == DeliveryType.GOSTANDARDED && shipmentDTO.RequestProcessingCountryId == 207)
                 {
                     throw new Exception("The GO STANDARD service is currently not available, please use the GO FASTER option");
                 }
