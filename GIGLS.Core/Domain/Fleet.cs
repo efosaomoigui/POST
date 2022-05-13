ff878fe270cff2d8ec1eaee7cb60e3804f8eaffe
Samuel Olanrewaju <olanrewaju.samuel@giglogistics.ng>
2022-05-13 07:59:40 +0100

    add enterprise partner id

--- GIGLS.Core/Domain/Fleet.cs
+++ GIGLS.Core/Domain/Fleet.cs
@@ -3,6 +3,7 @@ using GIGLS.Core;
 using GIGLS.Core.Domain.Partnership;
 using System.ComponentModel.DataAnnotations;
 using GIGLS.Core.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GIGL.GIGLS.Core.Domain
 {
@@ -27,5 +28,8 @@ namespace GIGL.GIGLS.Core.Domain
         public virtual Partner Partner { get; set; }
 
         public string FleetName { get; set; }
+        public string EnterprisePartnerId { get; set; }
+        [ForeignKey("EnterprisePartnerId")]
+        public virtual User EnterprisePartner { get; set; }
     }
 }
