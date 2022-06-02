70300356ef6e39eb9391224a909995de24d74cf1
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-06-02 23:59:59 +0100

    complete fleet dispute message

--- GIGLS.Core/IServices/Fleets/IFleetDisputeMessageService.cs
+++ GIGLS.Core/IServices/Fleets/IFleetDisputeMessageService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GIGLS.Core.DTO.Fleets;
 
 namespace GIGLS.Core.IServices.Fleets
 {
     public interface IFleetDisputeMessageService : IServiceDependencyMarker
     {
+        Task<bool> AddFleetDisputeMessageAsync(FleetDisputeMessageDto dto);
+        Task<List<FleetDisputeMessageDto>> GetAllFleetDisputeMessagesAsync();
     }
 }
--- GIGLS.Services/Implementation/Fleets/FleetDisputeMessageService.cs
+++ GIGLS.Services/Implementation/Fleets/FleetDisputeMessageService.cs
@@ -3,11 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GIGLS.Core;
+using GIGLS.Core.Domain;
+using GIGLS.Core.DTO.Fleets;
+using GIGLS.Core.DTO.MessagingLog;
+using GIGLS.Core.DTO.User;
+using GIGLS.Core.Enums;
+using GIGLS.Core.IMessageService;
+using GIGLS.Core.IServices;
 using GIGLS.Core.IServices.Fleets;
+using GIGLS.Core.IServices.Partnership;
+using GIGLS.Core.IServices.User;
+using GIGLS.Infrastructure;
 
 namespace GIGLS.Services.Implementation.Fleets
 {
     public class FleetDisputeMessageService : IFleetDisputeMessageService
     {
+        private readonly IUnitOfWork _uow;
+        private readonly IUserService _userService;
+        private readonly IMessageSenderService _messageSenderService;
+        private readonly IFleetPartnerService _fleetPartnerService;
+        private readonly IFleetService _fleetService;
+        private readonly ICaptainService _captainService;
+
+        public FleetDisputeMessageService(IUnitOfWork uow, IUserService userService, IMessageSenderService messageSenderService, IPartnerService partnerService, IFleetPartnerService fleetPartnerService, IFleetService fleetService, ICaptainService captainService)
+        {
+            _uow = uow;
+            _userService = userService;
+            _messageSenderService = messageSenderService;
+            _fleetPartnerService = fleetPartnerService;
+            _fleetService = fleetService;
+            _captainService = captainService;
+        }
+
+        public async Task<bool> AddFleetDisputeMessageAsync(FleetDisputeMessageDto dto)
+        {
+            try
+            {
+                var currentUser = await GetCurrentUserRoleAsync();
+
+                if (currentUser.SystemUserRole == "Administrator" || currentUser.SystemUserRole == "CaptainManagement")
+                {
+                    if (!(await _uow.Fleet.ExistAsync(c => c.RegistrationNumber.ToLower() == dto.VehicleNumber.Trim().ToLower())))
+                    {
+                        throw new GenericException($"Fleet/Vehicle with Registration Number: {dto.VehicleNumber} does not exist!");
+                    }
+
+                    var vehicleOwner = await _captainService.GetVehicleByRegistrationNumberAsync(dto.VehicleNumber);
+                    var fleetDisputeMessage = new FleetDisputeMessage()
+                    {
+                        DateModified = DateTime.Now,
+                        DateCreated = DateTime.Now,
+                        DisputeMessage = dto.DisputeMessage,
+                        VehicleNumber = dto.VehicleNumber,
+                        FleetOwnerId = vehicleOwner.VehicleOwnerId
+                    };
+                    _uow.FleetDisputeMessage.Add(fleetDisputeMessage);
+                    await _uow.CompleteAsync();
+
+                    var owner = await _userService.GetUserById(vehicleOwner.VehicleOwnerId);
+                    var passwordMessage = new PasswordMessageDTO()
+                    {
+                        Password = $"Dispute {dto.DisputeMessage} has been registered by the fleet partner {currentUser.FirstName} {currentUser.LastName}",
+                        UserEmail = owner.Email
+                    };
+
+                    await _messageSenderService.SendGenericEmailMessage(MessageType.SSC_Email, passwordMessage);
+                    await _messageSenderService.SendGenericEmailMessage(MessageType.CEMAIL, passwordMessage);
+
+                    return true;
+                }
+                throw new GenericException("You are not authorized to perform this operation");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<FleetDisputeMessageDto>> GetAllFleetDisputeMessagesAsync()
+        {
+            try
+            {
+                var currentUser = await GetCurrentUserRoleAsync();
+
+                if (currentUser.SystemUserRole == "Administrator" || currentUser.SystemUserRole == "CaptainManagement")
+                {
+                    var disputeMassages = _uow.FleetDisputeMessage.GetAll("FleetOwner").Select(x => new FleetDisputeMessageDto()
+                    {
+                        DisputeMessage = x.DisputeMessage,
+                        VehicleNumber = x.VehicleNumber,
+                        FleetOwnerId = x.FleetOwnerId,
+                    }).ToList();
+
+                    return disputeMassages;
+                }
+                throw new GenericException("You are not authorized to perform this operation");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private async Task<UserDTO> GetCurrentUserRoleAsync()
+        {
+            var currentUserId = await _userService.GetCurrentUserId();
+            var currentUser = await _userService.GetUserById(currentUserId);
+
+            return currentUser;
+        }
     }
 }
