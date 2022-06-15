4db01fd66a28901921bacb09dc6e7ee0c77704f4
Oladejo Sunday <oladejo.sunday@giglogistics.ng>
2022-06-15 19:24:13 +0100

    change captainId in FleetTrip and FleetTripDto

--- GIGLS.Core/Domain/FleetTrip.cs
+++ GIGLS.Core/Domain/FleetTrip.cs
@@ -1,5 +1,6 @@
 ﻿using GIGLS.Core.Domain;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GIGL.GIGLS.Core.Domain
 {
@@ -18,7 +19,9 @@ namespace GIGL.GIGLS.Core.Domain
         public int FleetId { get; set; }
         public virtual Fleet Fleet { get; set; }
         
-        public int CaptainId { get; set; }
+        public string CaptainId { get; set; }
+
+        [ForeignKey("CaptainId")]
         public virtual User Captain { get; set; }
     }
 }
